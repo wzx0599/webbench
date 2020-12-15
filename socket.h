4ed55ec545a103394c60414e9df7d1a03f6f79d9
wzx0599 <1145922655@qq.com>
2020-12-15 13:24:26 +0800

    构建初始化

---
+++ socket.h
@@ -0,0 +1,19 @@
+#ifndef WEBBENCH_SOCKET_H
+#define WEBBENCH_SOCKET_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int Socket(const char *host, int clientPort);
+
+#endif // WEBBENCH_SOCKET_H
